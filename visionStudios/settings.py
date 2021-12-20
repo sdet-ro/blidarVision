204389fd4f98310bb55d9ea2a378d31363e4c9de
jcodewriter <jcodewriter@gmail.com>
2021-12-20 04:54:12 -0800

    update setting

--- visionStudios/settings.py
+++ visionStudios/settings.py
@@ -149,9 +149,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'visionStudios/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,  'visionStudios/static')
+STATIC_ROOT = os.path.join(BASE_DIR,  'visionStudios')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FROM_USER = os.environ.get('EMAIL_FROM_USER')
