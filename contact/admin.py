47d941ff71cd30c488c9fd0210f80683d0db5865
AncaZpz <anca.zpz@gmail.com>
2022-01-19 22:13:34 +0200

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Post
-from contact.models import Application, Message, FloaterMessage
+from .models import Post, Application, Message, FloaterMessage
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Message)
