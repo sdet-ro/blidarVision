d925f9ba9978bd15be95a1825ab1178faaf617e4
AncaZpz <anca.zpz@gmail.com>
2022-01-20 15:38:41 +0200

    Update models.py

--- contact/models.py
+++ contact/models.py
@@ -1,22 +1,5 @@
 from django.db import models
 import datetime
-from django.contrib.auth.models import User
-
-STATUS = ((0,  "Draft"), (1, "Published"))
-
-class Post(models.Model):
-  title = models.CharField(max_lenght=1048)
-  slug = models.SlugField(max_lenght=1048)
-  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-  related_name=('blog_posts')
-  content = models.TextField()
-  status = models.IntegerField(choices=STATUS, default=0)
-
-  class Meta:
-      ordering = ['-created_on']
-
-  def __str__(self):
-    return self.title
 
 class Message(models.Model):
     email = models.EmailField(verbose_name="email", max_length=60)
