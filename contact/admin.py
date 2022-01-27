c12536554f60ed80c050153d13d9b209e630d894
AncaZpz <anca.zpz@gmail.com>
2022-01-27 13:23:03 +0200

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -5,3 +5,10 @@ admin.site.register(Post)
 admin.site.register(Message)
 admin.site.register(FloaterMessage)
 admin.site.register(Application)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fileds = ['title', 'content']
+
+admin.site.register(PostAdmin)    
