d414d5519380f6ad1060cbaa634b3f1e7094bb55
AncaZpz <anca.zpz@gmail.com>
2022-01-27 18:09:07 +0200

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Post, Application, Message, FloaterMessage
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Message)
 admin.site.register(FloaterMessage)
 admin.site.register(Application)
@@ -12,4 +11,4 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     search_fileds = ['title', 'content']
 
-admin.site.register(PostAdmin)    
+admin.site.register(Post, PostAdmin)    
